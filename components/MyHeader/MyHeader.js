653ac3dd295ce26415bc2f8fb34dfe3dd6f74c6e
alexeyShuncko <shuncko.a@yandex.ru>
2023-06-05 17:56:42 +0300

    nav

--- components/MyHeader/MyHeader.js
+++ components/MyHeader/MyHeader.js
@@ -1,22 +1,41 @@
+'use client';
 import Link from 'next/link';
 import s from './MyHeader.module.css';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 export const MyHeader = () => {
+  const pathname = usePathname();
   return (
     <header className={s.myHeader}>
+      {/* <Image src={logo} alt="Логотип" width={50} height={20} /> */}
       <ul>
         <li>
-          <Link href={'/'}>Главная</Link>
+          <Link href={'/'} className={pathname === '/' ? s.activeLink : ''}>
+            Главная
+          </Link>
         </li>
         <li>
-          <Link href={'/projects'}>Проекты</Link>
+          <Link
+            href={'/projects'}
+            className={pathname === '/projects' ? s.activeLink : ''}>
+            Проекты
+          </Link>
         </li>
         <li>
-          <Link href={'/about'}>О компании</Link>
+          <Link
+            href={'/about'}
+            className={pathname === '/about' ? s.activeLink : ''}>
+            О компании
+          </Link>
         </li>
 
         <li>
-          <Link href={'/contacts'}>Контакты</Link>
+          <Link
+            href={'/contacts'}
+            className={pathname === '/contacts' ? s.activeLink : ''}>
+            Контакты
+          </Link>
         </li>
       </ul>
     </header>
